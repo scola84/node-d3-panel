daf55e21783c19c3a75230c7c13b146a4bf355a9
M.C. van den Boogaart <scola@kvdb.name>
2016-09-29 22:11:19 +0200

    Update scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
+  dest: './dist/d3-panel.js',
   entry: 'index.js',
   format: 'umd',
-  globals: {
-    'd3-selection': 'd3'
-  },
+  moduleName: 'd3',
   plugins: [
+    resolve({
+      jsnext: true
+    }),
     buble()
   ]
 };
